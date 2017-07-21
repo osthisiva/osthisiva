d9967e71aebd2f8a8c4ff6ea69a1b58fd69424f9
osthisiva <30342957+osthisiva@users.noreply.github.com>
2017-07-21 17:06:23 +0530

    Create osthisiva.c

---
+++ osthisiva.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+voidmain()
+{
+int a;
+printf("enter the value ");
+scanf("%d",&a);
+if(a>0)
+printf("the number is positive");
+if else(a<0)
+{
+printf("the number is negative");
+else
+printf("the number is zero");
+}}
